9fcb136978f2e59afa795274d71e432ca6421fdd
Trần Công Minh <minh.trancong@metropolia.fi>
2022-08-05 13:18:43 +0300

    asd

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const logger = require('./utils/logger')
 const workspaceRouter = require('./controllers/workspace')
 const setupWorkspaceRouter = require('./controllers/setupWorkspace')
 const path = require('path')
-// const schedule = require('node-schedule')
+const schedule = require('node-schedule')
 
 logger.info('connecting to', config.MONGODB_URI)
 mongoose.connect(config.MONGODB_URI)
